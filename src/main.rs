1e543f9da9bbf3aa8fa3c89a7c589c01f17f0e64
borfus <mrpeteyb@gmail.com>
2021-07-01 23:49:29 -0500

    Added ssl decryption

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::net::{TcpListener, TcpStream};
 use std::{fs, env, process, str};
 use std::path::Path;
 use std::vec::Vec;
+use std::sync::Arc;
+
+use openssl::ssl::{SslMethod, SslAcceptor, SslStream, SslFiletype};
 
 #[macro_use]
 extern crate clap;
@@ -11,7 +14,9 @@ use clap::ArgMatches;
 
 struct Config {
     root: String,
-    port: String
+    port: String,
+    key: Option<String>,
+    cert: Option<String>
 }
 
 impl Config {
@@ -28,7 +33,23 @@ impl Config {
         };
         let port = String::from(port);
 
-        Ok(Config{root, port})
+        let key = match args.value_of("KEY") {
+            Some(key) => Some(String::from(key)),
+            None if port == "443" => {
+                panic!("Command line argument -k or --key is required if using port 443!");
+            },
+            None => None
+        };
+
+        let cert = match args.value_of("CERT") {
+            Some(cert) => Some(String::from(cert)),
+            None if port == "443" => {
+                panic!("Command line argument -c or --cert is required if using port 443!");
+            },
+            None => None
+        };
+
+        Ok(Config{root, port, key, cert})
     }
 }
 
@@ -37,8 +58,10 @@ fn main() {
         (version: "1.0")
         (author: "Peter Bell")
         (about: "Basic HTTP Server")
-        (@arg PORT: -p --port +takes_value "Port that the server will bind to")
-        (@arg ROOT: +required "Website root directory")
+        (@arg PORT: -p --port +takes_value "Port that the server will bind to.")
+        (@arg KEY: -k --key +takes_value "Directory to SSL private key PEM file. Required if using port 443!")
+        (@arg CERT: -c --cert +takes_value "Directory to SSL certificate PEM file. Required if using port 443!")
+        (@arg ROOT: +required "Website root directory.")
     ).get_matches();
 
     let config = Config::new(args).unwrap_or_else(|err| {
@@ -46,16 +69,40 @@ fn main() {
         process::exit(1);
     });
 
-    let listener = TcpListener::bind(format!("{}{}", "127.0.0.1:", &config.port[..])).unwrap();
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", &config.port[..])).unwrap();
 
     let root = Path::new(&config.root[..]);
     assert!(env::set_current_dir(&root).is_ok());
+
+    let mut key_present = false;
+    let mut key_string = String::new();
+    if let Some(key) = config.key{
+        key_present = true;
+        key_string = String::from(key);
+    };
+
+    let mut cert_string = String::new();
+    if key_present {
+        if let Some(cert) = config.cert {
+            cert_string = String::from(cert);
+        };
+    }
+
+    let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    if key_present {
+        acceptor.set_private_key_file(key_string, SslFiletype::PEM).unwrap();
+        acceptor.set_certificate_chain_file(cert_string).unwrap();
+        acceptor.check_private_key().unwrap();
+    }
+    let acceptor = Arc::new(acceptor.build());
 
     let pool = ThreadPool::new(5);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        pool.execute(|| {
+        let acceptor = acceptor.clone();
+        pool.execute(move || {
+            let stream = acceptor.accept(stream).unwrap();
             handle_connection(stream);
         });
     }
@@ -63,17 +110,20 @@ fn main() {
     println!("Shutting down.");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+fn handle_connection(mut stream: SslStream<TcpStream>) {
+    let mut buffer = [0; 2048];
     stream.read(&mut buffer).unwrap();
 
-    let buffer = str::from_utf8(&buffer).unwrap().to_string();
-    let buffer : Vec<&str> = buffer.split_whitespace().collect();
-    let mut uri = buffer[1];
+    println!("{}", String::from_utf8_lossy(&buffer));
+
+    let buffer = String::from_utf8_lossy(&buffer);
+    let buffer: Vec<&str> = buffer.split_whitespace().collect();
+    let mut uri = buffer[1].to_string();
 
     if uri == "/" {
-        uri = "/index.html";
+        uri = "/index.html".to_string();
     }
+    println!("{}", uri);
 
     let uri = uri.replace("%20", " ");
 
